3d1f0e28fb8899b56ef8e4df51481a80942b3f34
prakash1329 <you@example.com>
2020-08-01 22:21:53 -0700

    Data ingesting queries

---
+++ Creating New BigQuery Datasets and Visualizing Insights/Ingesting New Datasets into BigQuery v1.5.sql
@@ -0,0 +1,17 @@
+"""
+Ingest a new dataset from a Google Spreadsheet
+"""
+
+#standardSQL
+SELECT
+  *,
+  SAFE_DIVIDE(orderedQuantity,stockLevel) AS ratio
+FROM
+  ecommerce.products
+WHERE
+# include products that have been ordered and
+# are 80% through their inventory
+orderedQuantity > 0
+AND SAFE_DIVIDE(orderedQuantity,stockLevel) >= .8
+ORDER BY
+  restockingLeadTime DESC
